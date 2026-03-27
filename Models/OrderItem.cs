b16788a76f45f10214d1c9659d2bb7589be2082f
Maya Arafa <mayaarafa@Mayas-MacBook-Pro.local>
2026-03-26 17:06:58 -0700

    setup and seed database

---
+++ Models/OrderItem.cs
@@ -0,0 +1,9 @@
+namespace Bookstore.Models
+{
+    public class OrderItem
+    {
+        public Book Book { get; set; }
+        public int Quantity { get; set; }
+        public decimal GetTotal() => Book.Price * Quantity;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Bookstore.Data;
 using Bookstore.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +8,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// setup database context
+var connectionString = builder.Configuration.GetConnectionString("BOOKSTORE_DB") ?? throw new InvalidOperationException("Connection string 'BOOKSTORE_DB' not found.");
+builder.Services.AddDbContextFactory<BookstoreDb>(options =>
+    options.UseSqlServer(connectionString, sqlServerOptions => 
+        sqlServerOptions.EnableRetryOnFailure(
+            maxRetryCount: 10,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null
+        )
+    ) 
+);
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -14,6 +39,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
